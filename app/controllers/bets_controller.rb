811b6c599286c3992ca332eb3f00f002035ba672
Alex-Harron <aharron.harron@gmail.com>
2021-04-28 19:22:44 -0400

    building out bets controller

--- app/controllers/bets_controller.rb
+++ app/controllers/bets_controller.rb
@@ -1,2 +1,21 @@
 class BetsController < ApplicationController
+    before_action :set_bet, except: [:index, :new, :create]
+
+    def index
+        if params[:game_id] && @game = Game.find_by(id: parasm[:game_id])
+            @bet = @game.bets 
+        else
+            @bets = Bet.all
+        end
+    end
+
+    def new 
+        if params[:game_id]
+            @game = Game.find_by(id: params[:game_id])
+            @bet = @game.bets.build
+        else
+            @bet = Bet.new
+            @bet.build_game
+        end
+    end
 end
