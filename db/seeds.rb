4a7db354d65cf9f8ab2da04626f4c8c871768a54
Alex-Harron <aharron.harron@gmail.com>
2021-05-05 19:49:57 -0400

    updating views

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,4 @@ Game.create(name: 'Mavricks V Clippers', home: 'Mavricks', visitor: 'Clippers', 
 Game.create(name: 'Heat V Magic', home: 'Mavricks', visitor: 'Clippers', home_img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Dallas_Mavericks_logo.svg/1200px-Dallas_Mavericks_logo.svg.png', visitor_img: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Los_Angeles_Clippers_%282015%29.svg/1024px-Los_Angeles_Clippers_%282015%29.svg.png')
 Game.create(name: 'Grizzlys V Bucks', home: 'Mavricks', visitor: 'Clippers', home_img: 'https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Dallas_Mavericks_logo.svg/1200px-Dallas_Mavericks_logo.svg.png', visitor_img: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Los_Angeles_Clippers_%282015%29.svg/1024px-Los_Angeles_Clippers_%282015%29.svg.png')
 
-User.delete_all
+Bet.delete_all
